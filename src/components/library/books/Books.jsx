9e500c31f202c85948f14a3a526f2a62da2d87db
facundolgomez <facundolgomez87@gmail.com>
2025-08-11 11:51:31 -0300

    ordeno carpetas relacionadas a books dentro de library

---
+++ src/components/library/books/Books.jsx
@@ -0,0 +1,47 @@
+import BookItem from "../bookItem/BookItem";
+import { useState } from "react";
+import BookSearch from "../bookSearch/BookSearch";
+const Books = ({ books }) => {
+  const [selectedBook, setSelectedBook] = useState("");
+  const [bookSearch, setBookSearch] = useState("");
+
+  const handleBookSearch = (book) => {
+    setBookSearch(book);
+  };
+
+  const booksToShow = books.filter((book) =>
+    book.title.toLowerCase().includes(bookSearch.toLowerCase())
+  );
+  return (
+    <>
+      {selectedBook && (
+        <p>
+          El libro seleccionado es <b>{selectedBook}</b>
+        </p>
+      )}
+      <BookSearch onFindBook={handleBookSearch} value={bookSearch} />
+      <div className="d-flex justify-content-center flex-wrap">
+        {booksToShow.length > 0 ? (
+          booksToShow.map((book) => {
+            return (
+              <BookItem
+                key={book.id}
+                title={book.title}
+                author={book.author}
+                rating={book.rating}
+                pageCount={book.pageCount}
+                imageUrl={book.imageUrl}
+                available={book.available}
+                onSelectedBook={setSelectedBook}
+              />
+            );
+          })
+        ) : (
+          <h2>No se encontraron lecturas con ese nombre</h2>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Books;
