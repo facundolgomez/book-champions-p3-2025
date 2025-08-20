bd97dcfb48e5b1a1a3a7f349fa251eeb1f6895ff
Facundo Gomez <facundolgomez87@gmail.com>
2025-08-20 12:53:13 -0300

    agrego propiedad summary a los libros y ruta dinamica

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import Login from "../../components/auth/login/Login";
 import { Button } from "react-bootstrap";
 import { Route, useNavigate, Routes } from "react-router";
+import BookDetails from "../library/bookDetails/BookDetails";
 
 const Dashboard = ({ onLogout }) => {
   const books = [
@@ -17,6 +18,8 @@ const Dashboard = ({ onLogout }) => {
       imageUrl:
         "https://acdn-us.mitiendanube.com/stores/001/542/126/products/9789878000107-b82c22cfb174dca93016944484618644-1024-1024.jpg",
       available: true,
+      summary:
+        "Un niño huérfano descubre que es un mago y comienza su educación en Hogwarts, enfrentándose a sus primeros desafíos mágicos.",
     },
     {
       id: 2,
@@ -27,6 +30,8 @@ const Dashboard = ({ onLogout }) => {
       imageUrl:
         "https://images.cdn1.buscalibre.com/fit-in/360x360/66/1a/661a3760157941a94cb8db3f5a9d5060.jpg",
       available: true,
+      summary:
+        "Un grupo de héroes emprende un viaje épico para destruir un anillo de poder maligno que amenaza con dominar la Tierra Media.",
     },
     {
       id: 3,
@@ -37,6 +42,8 @@ const Dashboard = ({ onLogout }) => {
       imageUrl:
         "https://images.cdn2.buscalibre.com/fit-in/360x360/0d/73/0d739e6e0e837d7637f97f9aad3639b4.jpg",
       available: true,
+      summary:
+        "En un planeta desértico donde la especia es la sustancia más valiosa del universo, un joven se convierte en el líder de una rebelión que cambiará el destino de todos.",
     },
     {
       id: 4,
@@ -47,6 +54,8 @@ const Dashboard = ({ onLogout }) => {
       imageUrl:
         "https://images.cdn1.buscalibre.com/fit-in/360x360/b0/39/b039af065268818b7bd3b0e016f8db65.jpg",
       available: true,
+      summary:
+        "En un mundo gobernado por un régimen totalitario, un hombre lucha contra la vigilancia constante y la manipulación de la verdad.",
     },
   ];
   const handleBookAdded = (book) => {
@@ -114,6 +123,7 @@ const Dashboard = ({ onLogout }) => {
           path="add-book"
           element={<NewBook onBookAdded={handleBookAdded} />}
         />
+        <Route path=":id" element={<BookDetails />} />
       </Routes>
 
       {/* <Login /> */}
